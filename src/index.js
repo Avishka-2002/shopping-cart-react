f4279ecc23530ee89b1becdb6edf242d285df872
Avishka <avishka2002sanjeewa@gmail.com>
2026-04-02 14:25:25 +0530

    fix tailwind error

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css'; // Make sure this file exists!
 import App from './App';
-import { CartProvider } from './context/CartContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <CartProvider>
-      <App />
-    </CartProvider>
+    <App />
   </React.StrictMode>
 );
